44f425627036363aaacd363d89b5039206643fa8
mhjmhj2002 <mhjmhj2002@gmail.com>
2023-04-25 10:21:34 -0300

    add source

---
+++ src/main/java/com/mhj/math/MhjMathBackApplication.java
@@ -0,0 +1,13 @@
+package com.mhj.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhjMathBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhjMathBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhj/math/MhjMathBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhj.math;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhjMathBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
